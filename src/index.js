37d9c97c9847f26e816c2f86796f78ea03062aff
mengshalitsofi <mengshalitsofi@gmail.com>
2021-04-28 19:50:48 -0700

    submit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code  here
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    const task = document.getElementById("new-task-description").value;
+
+    const li = document.createElement("li");
+    li.innerText = task;
+
+    document.getElementById("tasks").appendChild(li);
+    
+    e.preventDefault();
+  })
 });
